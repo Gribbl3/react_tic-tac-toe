2a52e40290765d2a8b1cd92eeafdea01e5737bb2
Gribbl3 <allenjoseph.lozada.21@usjr.edu.ph>
2024-01-02 21:22:22 +0800

    final commit

--- tic_tac_toe/src/Board.tsx
+++ tic_tac_toe/src/Board.tsx
@@ -1,5 +1,10 @@
-import { useState } from "react";
 import Square from "./Square";
+
+interface BoardProps{
+  squares: string[];
+  xIsNext: boolean;
+  onPlay: (newSquares:string[]) => void;
+}
 
 function calculateWinner(squares: string[]): string | null {
   const lines = [
@@ -27,49 +32,44 @@ function calculateWinner(squares: string[]): string | null {
 }
 
 
-function Board() {
-  const [squares, setSquares] = useState<string[]>(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState<boolean>(true);
+function Board(props: BoardProps) {
   
-  const winner = calculateWinner(squares);
-  const status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? "X" : "O"}`;
+  const winner = calculateWinner(props.squares);
+  const status = winner ? `Winner: ${winner}` : `Next player: ${props.xIsNext ? "X" : "O"}`;
 
   function handleClick(index:number) {
     //base case 
-    if(calculateWinner(squares) || squares[index]) return;
+    if(calculateWinner(props.squares) || props.squares[index]) return;
 
     //create a copy of the squares array
-    const newSquares = squares.slice();
+    const newSquares = props.squares.slice();
     //set the value of the square that was clicked
-    newSquares[index] = xIsNext ? "X" : "O";
+    newSquares[index] = props.xIsNext ? "X" : "O";
     //update the squares array with the new value
-    setSquares(newSquares);
-    //set the next player
-    setXIsNext(!xIsNext);
+    props.onPlay(newSquares);
   }
 
   return (
     <>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={squares[0]} onClick={() => handleClick(0)} />
-        <Square value={squares[1]} onClick={() => handleClick(1)} />
-        <Square value={squares[2]} onClick={() => handleClick(2)} />
+        <Square value={props.squares[0]} onClick={() => handleClick(0)} />
+        <Square value={props.squares[1]} onClick={() => handleClick(1)} />
+        <Square value={props.squares[2]} onClick={() => handleClick(2)} />
       </div>
 
       <div className="board-row">
-        <Square value={squares[3]} onClick={() => handleClick(3)} />
-        <Square value={squares[4]} onClick={() => handleClick(4)} />
-        <Square value={squares[5]} onClick={() => handleClick(5)} />
+        <Square value={props.squares[3]} onClick={() => handleClick(3)} />
+        <Square value={props.squares[4]} onClick={() => handleClick(4)} />
+        <Square value={props.squares[5]} onClick={() => handleClick(5)} />
       </div>
 
       <div className="board-row">
-        <Square value={squares[6]} onClick={() => handleClick(6)} />
-        <Square value={squares[7]} onClick={() => handleClick(7)} />
-        <Square value={squares[8]} onClick={() => handleClick(8)} />
+        <Square value={props.squares[6]} onClick={() => handleClick(6)} />
+        <Square value={props.squares[7]} onClick={() => handleClick(7)} />
+        <Square value={props.squares[8]} onClick={() => handleClick(8)} />
       </div>
 
-      <button className="reset-btn" onClick={() => {setSquares(Array(9).fill(null)),setXIsNext(true)}}>Reset</button>
 
     </>
   );
--- tic_tac_toe/src/Game.tsx
+++ tic_tac_toe/src/Game.tsx
@@ -1,16 +1,49 @@
-import Board from './Board'
+import Board from "./Board";
+import { useState } from "react";
 
 function Game() {
+  //2d string array to store the history of the game
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+  const [history, setHistory] = useState<string[][]>([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares: string[]) {
+    //if no spread operator, history will be like this:
+    //[[a,b], a, b]
+    //with spread operator, history will be like this:
+    //[a,b,a,b]
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove: number) {
+    setHistory(history.slice(0, nextMove + 1));
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((step, move) => {
+    let description;
+    move > 0
+      ? (description = `Go to move #${move}`)
+      : (description = "Go to game start");
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="game">
-        <div className="game-board">
-            <Board/>
-        </div>
-        <div className="game-info">
-            <ol></ol>
-        </div>
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Game
+export default Game;
